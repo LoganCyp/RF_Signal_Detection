c16b1d3ed8c710f1c9a5de3deb2680cc4644e8f6
Logan Cypser <loganmatthewcyp@gmail.com>
2025-05-17 13:54:52 -0400

    Added energy threshold for steady state prediction

--- EDA/plot_signal.py
+++ EDA/plot_signal.py
@@ -2,9 +2,12 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-filepath = r"D:\CARDRF\CARDRF\LOS\Test\UAV\BEEBEERUN\FLYING\BEEBEERUN_0000100002.mat"  
+filepath = r"D:\CARDRF\CARDRF\LOS\Test\UAV\DJI_INSPIRE\FLYING\INSPIRE_10M_FLYING_ABOVE00001.mat"  
 # scale from digital to voltage as spec'd in paper
 scale_factor = 6.581e-6 
+
+# Length of steady state sample (using 1024 as per specs)
+steady_length = 1024
 
 
 with h5py.File(filepath, 'r') as f:
@@ -12,12 +15,33 @@ with h5py.File(filepath, 'r') as f:
     signal = signal.squeeze() * scale_factor
 
 
+window_size=100
+threshold_ratio=0.5
+    
+energy = np.convolve(signal**2, np.ones(window_size), mode='valid')
+threshold = np.max(energy) * threshold_ratio
+candidates = np.where(energy > threshold)[0]
+
+start = candidates[0]
+end = min(start + steady_length, len(signal))
 
 plt.figure(figsize=(10, 4))
 plt.plot(signal)
-plt.title("Beebeerun Sample Signal")
+plt.axvline(start, color='green', linestyle='--', label="Steady Start")
+plt.axvline(end, color='red', linestyle='--', label="Steady End")
+plt.title("DJI Inspire Sample Signal")
 plt.xlabel("Sample Index")
 plt.ylabel("Voltage (V)")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+plt.figure(figsize=(10, 3))
+plt.plot(signal[start:end])
+plt.title("DJI Inspire Cropped 1024 Samples")
+plt.xlabel("Sample Index")
+plt.ylabel("Voltage (V)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
